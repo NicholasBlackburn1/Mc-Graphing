09d85296d3422221276073b0cbc49694dc042894
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-05-15 19:09:33 -0400

    trying to make a convert class

---
+++ src/main/java/graphing/drawing/GraphingData.java
@@ -0,0 +1,12 @@
+/**
+ * This class is for converting data into minecraft Vertex form
+ * @author nicholas Blackburn
+ */
+
+package graphing.drawing;
+
+public class GraphingData {
+    
+
+    
+}
